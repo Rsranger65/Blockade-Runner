40a9bf3a8d65b794bee454d7edb54d1e51faac3f
Alex Gittemeier <algitt65@gmail.com>
2015-01-04 01:11:49 -0600

    Stubs for AudioManager and SettingsManager

---
+++ src/net/kopeph/ld31/SettingsManager.java
@@ -0,0 +1,13 @@
+package net.kopeph.ld31;
+
+/**
+ * @author alexg
+ */
+public class SettingsManager {
+
+	public SettingsManager() {}
+	public void set(String key, String value) {}
+	public String get(String key) { return "NOTIMPLEMENTED"; }
+	public int getInt(String key) { return -1; }
+	public float getFloat(String key) { return Float.NaN; }
+}
