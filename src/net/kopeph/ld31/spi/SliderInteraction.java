0aa8d11e707e75db810b5d96d2e0b9c7e8c9a58a
Alex Gittemeier <algitt65@gmail.com>
2015-04-02 20:31:52 -0500

    Edit name of SilderPredicate to match code style

--- src/net/kopeph/ld31/spi/SliderInteraction.java
+++ src/net/kopeph/ld31/spi/SliderInteraction.java
@@ -4,6 +4,7 @@ package net.kopeph.ld31.spi;
  * Referenced in
  * net.kopeph.ld31.menu.Slider
  */
-public interface SliderPredicate {
+@FunctionalInterface
+public interface SliderInteraction {
 	public void update(float value);
 }
